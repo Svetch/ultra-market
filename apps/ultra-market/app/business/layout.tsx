d1a5a54e178ad10337c58138119d6b00b79a3295
NgergoN <nagygeee@hotmail.com>
2024-04-21 15:20:50 +0200

    Fix build errors

--- apps/ultra-market/app/business/layout.tsx
+++ apps/ultra-market/app/business/layout.tsx
@@ -5,13 +5,9 @@ import Footer from "../../components/layout/footer";
 export const runtime = "edge";
 
 export default function Layout({children}: { children: React.ReactNode }) {
-  const navItems = [
-    {children: 'About', href: '/about'},
-    {children: 'Contact', href: '/contact'},
-  ];
   return (
     <div className="grid grid-rows-[auto,1fr,auto] min-h-screen w-full">
-      <Navbar items={navItems} logo={Logo}/>
+      <Navbar logo={Logo}/>
       <main className="grid">{children}</main>
       <Footer companyName={'Ultra-Market'}/>
     </div>
