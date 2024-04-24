c7c56bd70d6ccc0abe8c54b9a877e517c84379d2
Svetch <svetch.game@gmail.com>
2024-04-24 06:43:06 +0200

    fix: fix db access and add otel integration

--- apps/ultra-market/app/api/[[...route]]/db.ts
+++ apps/ultra-market/app/api/[[...route]]/db.ts
@@ -1,11 +1,10 @@
+import 'server-only';
 import { PrismaClient } from '@prisma/client';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
-
-const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-const adapter = new PrismaNeon(neon);
-
-export const db = new PrismaClient({ adapter });
+export const createDbConnection = () => {
+  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
--- apps/ultra-market/app/api/[[...route]]/route.ts
+++ apps/ultra-market/app/api/[[...route]]/route.ts
@@ -1,34 +1,63 @@
-import { db } from './db';
+import { BaselimeLogger } from '@baselime/edge-logger';
+import { PrismaClient } from '@prisma/client';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { createDbConnection } from './db';
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api');
+type Environment = {
+  DB: () => PrismaClient;
+  BASELIME_API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Environment }>().basePath('/api');
+// init baselime
+app.use(async (ctx, next) => {
+  const logger = new BaselimeLogger({
+    service: 'ultra-market',
+    namespace: ctx.req.url,
+    apiKey: ctx.env?.BASELIME_API_KEY || process.env.BASELIME_API_KEY!,
+    ctx: ctx.executionCtx,
+    isLocalDev: process.env.NODE_ENV === 'development',
+  });
+  await next();
+  if (ctx.executionCtx.waitUntil) ctx.executionCtx.waitUntil(logger.flush());
+  else {
+    await logger.flush();
+  }
+});
+
+//init prisma
+app.use(async (ctx, next) => {
+  ctx.env.DB = createDbConnection;
+  await next();
+});
 
 app.get('/search/:query?', async (ctx, params) => {
+  const DB = ctx.env.DB();
   const { query } = ctx.req.param();
-  const items = await db.shopItem.findMany({
+  const items = await DB.shopItem.findMany({
     where: {
-        OR:[
-            {
-              description: {
-                contains: query,
-                mode: 'insensitive',
-              },
-            },
-            {
-              name: {
-                contains: query,
-                mode: 'insensitive',
-              }
-            },
-            {
-              tags: {
-                hasSome: query?.split(' '),
-              }
-            }
-          ]
+      OR: [
+        {
+          description: {
+            contains: query,
+            mode: 'insensitive',
+          },
+        },
+        {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          },
+        },
+        {
+          tags: {
+            hasSome: query?.split(' '),
+          },
+        },
+      ],
     },
     take: 10,
     select: {
